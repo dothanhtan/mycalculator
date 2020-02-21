4e4409755658a3d592b321b766a861d9d709b343
thanhtan28011998 <thanhtan28011998@gmail.com>
2020-02-21 17:08:08 +0700

    ti le vang

---
+++ src/components/button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = (props) => {
+    
+    return (
+        
+        <input
+            className={props.className}
+            type="button"
+            value={props.label}
+            onClick = {props.handleClick}
+        />
+    );
+}
+
+export default Button;
